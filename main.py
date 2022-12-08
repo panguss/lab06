d4566c6b278cc4eba6fd4b10ee5eac5e24ac55f8
panguss <113355417+panguss@users.noreply.github.com>
2022-12-08 10:50:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-import numpy as np  # Нужен для создания матрицы
-import pandas as pd  # нужен для нормальной работы numpy
+from statistics import geometric_mean
+n = int(input('Введите количество критериев (целое число): '))
+m = []
+for i in range(n):
+    g = []
+    for j in range(n):
+        if i == j:
+            a = 1
+        elif i > j:
+            a = 1 / m[j][i]
+        else:
+            while True:
+                try:
+                    a = float(input(f'Введите отношение критерия {i + 1} к критерию {j + 1}: '))
+                    break
+                except ValueError:
+                    print('Ошибка ')
+        g.append(a)
+    m.append(g)
+s = 0
+sz = []
 
-# Пользователь вводит количество критериев
-while True:
-    try:
-        number = int(input('Сколько критериев Вы желаете рассмотреть?\n''Введите количество: '))
-    except ValueError as e:
-        print("Введите число, а не текст!")
-    else:
-        break
+for c in range(n):
+    r = geometric_mean(m[c])
+    s += r
+    sz.append(r)
 
-def matrix(n):
-    # Создаем матрицу
-    M = np.ones([n, n])
-    for i in range(0, n):
-        for j in range(0, n):
-            if i < j:
-                while True:
-                    try:
-                        mij = input(f'\nНасколько критерий {i}  приоритетнее критерия {j}?\n''Введите коэффициент: ')
-                    except ValueError as e:
-                        print("Введите число, а не текст/ноль!")
-                    else:
-                        break
-
-                M[i, j] = float(mij)
-                M[j, i] = 1 / float(mij)  # Добавление обратных элементов (под главной диагональю)
-
-    # Чтобы вывести весовые коэффициенты, необходимо вычислить собственный вектор матрицы М.
-    # Для этого воспользуемся функцией numpy.linalg.eig(М)[1][:,0]
-
-    vector = np.linalg.eig(M)[1][:, 0]
-    # пронормируем вектор
-    norm_vector = vector / vector.sum()
-    return norm_vector
-
-
-norm_vector = matrix(number)
-# выводим результат
-for x in range(len(norm_vector)):
-    res = norm_vector[x]
-    print(f'Коэффициент критерия {x}: {res:.2f}')  # округление до сотых
+for z in range(n):
+    weight = sz[z] / s
+    print(f'Весовой коэффициент {z + 1} критерия: {round(weight, 2)}')
